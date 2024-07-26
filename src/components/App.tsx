86ae770458df558f374d18ae6065c7ceddcd3cec
savameta <hoando.dev@gmail.com>
2024-07-26 16:15:51 +0700

    update ui

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,18 +1,14 @@
-import { useIntegration } from "@telegram-apps/react-router-integration";
 import {
   bindMiniAppCSSVars,
   bindThemeParamsCSSVars,
   bindViewportCSSVars,
-  initNavigator,
   useLaunchParams,
   useMiniApp,
   useThemeParams,
   useViewport,
 } from "@telegram-apps/sdk-react";
-import { AppRoot } from "@telegram-apps/telegram-ui";
-import { type FC, useEffect, useMemo } from "react";
-import { Navigate, Route, Router, Routes } from "react-router-dom";
-import { routes } from "../navigation/routes";
+import { type FC, useEffect } from "react";
+import { IndexPage } from "../pages/IndexPage/IndexPage";
 
 export const App: FC = () => {
   const lp = useLaunchParams();
@@ -34,29 +30,9 @@ export const App: FC = () => {
 
   // Create a new application navigator and attach it to the browser history, so it could modify
   // it and listen to its changes.
-  const navigator = useMemo(() => initNavigator("app-navigation-state"), []);
-  const [location, reactNavigator] = useIntegration(navigator);
 
   // Don't forget to attach the navigator to allow it to control the BackButton state as well
   // as browser history.
-  useEffect(() => {
-    navigator.attach();
-    return () => navigator.detach();
-  }, [navigator]);
 
-  return (
-    <AppRoot
-      appearance={miniApp.isDark ? "dark" : "light"}
-      platform={["macos", "ios"].includes(lp.platform) ? "ios" : "base"}
-    >
-      <Router location={location} navigator={reactNavigator}>
-        <Routes>
-          {routes.map((route) => (
-            <Route key={route.path} {...route} />
-          ))}
-          <Route path="*" element={<Navigate to="/" />} />
-        </Routes>
-      </Router>
-    </AppRoot>
-  );
+  return <IndexPage />;
 };
--- src/components/Root.tsx
+++ src/components/Root.tsx
@@ -1,6 +1,5 @@
 import { SDKProvider, useLaunchParams } from "@telegram-apps/sdk-react";
-import { TonConnectUIProvider } from "@tonconnect/ui-react";
-import { type FC, useEffect, useMemo } from "react";
+import { type FC, useEffect } from "react";
 
 import { ErrorBoundary } from "./ErrorBoundary";
 import { App } from "./App";
@@ -22,9 +21,6 @@ const ErrorBoundaryError: FC<{ error: unknown }> = ({ error }) => (
 
 const Inner: FC = () => {
   const debug = useLaunchParams().startParam === "debug";
-  const manifestUrl = useMemo(() => {
-    return new URL("tonconnect-manifest.json", window.location.href).toString();
-  }, []);
 
   // Enable debug mode to see all the methods sent and events received.
   useEffect(() => {
@@ -34,11 +30,9 @@ const Inner: FC = () => {
   }, [debug]);
 
   return (
-    <TonConnectUIProvider manifestUrl={manifestUrl}>
-      <SDKProvider acceptCustomStyles debug={debug}>
-        <App />
-      </SDKProvider>
-    </TonConnectUIProvider>
+    <SDKProvider acceptCustomStyles debug={debug}>
+      <App />
+    </SDKProvider>
   );
 };
 
