dfcb56cf979c343bee00cd55be08a0aa04b95cdc
savameta <hoando.dev@gmail.com>
2024-07-26 14:00:55 +0700

    update responsive

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,8 @@
 import { useInitData, useLaunchParams } from "@telegram-apps/sdk-react";
-import "./App.css";
+import { initViewport, requestViewport } from "@telegram-apps/sdk";
 import { useState, useEffect } from "react";
 import { Unity, useUnityContext } from "react-unity-webgl";
+import "./App.css";
 
 function App() {
   const { unityProvider } = useUnityContext({
@@ -13,7 +14,6 @@ function App() {
 
   const initDataRaw = useLaunchParams().initDataRaw;
   const initData = useInitData();
-
   console.log(initDataRaw);
 
   const [devicePixelRatio, setDevicePixelRatio] = useState(
@@ -26,12 +26,48 @@ function App() {
   });
 
   useEffect(() => {
-    // Function to update the device pixel ratio
+    const init = async () => {
+      const viewportPromise = initViewport();
+      const viewport = await viewportPromise[0];
+      console.log(viewport);
+
+      const updateDimensions = (data: any) => {
+        setDimensions({
+          width: data.width,
+          height: data.height,
+        });
+      };
+
+      const viewportData = await requestViewport();
+      updateDimensions(viewportData);
+
+      viewport.on("change:height", (height) => {
+        setDimensions((prev) => ({
+          ...prev,
+          height,
+        }));
+      });
+
+      viewport.on("change:width", (width) => {
+        setDimensions((prev) => ({
+          ...prev,
+          width,
+        }));
+      });
+
+      viewport.on("change:isExpanded", (isExpanded) => {
+        if (isExpanded) {
+          // Handle expansion logic if necessary
+        }
+      });
+    };
+
+    init();
+
     const updateDevicePixelRatio = () => {
       setDevicePixelRatio(window.devicePixelRatio);
     };
 
-    // Function to update the dimensions of the Unity component
     const updateDimensions = () => {
       setIsMobile(window.innerWidth <= 768);
       if (isMobile) {
@@ -47,16 +83,13 @@ function App() {
       }
     };
 
-    // Media matcher for device pixel ratio changes
     const mediaMatcher = window.matchMedia(
       `screen and (resolution: ${devicePixelRatio}dppx)`
     );
     mediaMatcher.addEventListener("change", updateDevicePixelRatio);
 
-    // Event listener for window resize
     window.addEventListener("resize", updateDimensions);
 
-    // Initial dimensions setup
     updateDimensions();
 
     return () => {
