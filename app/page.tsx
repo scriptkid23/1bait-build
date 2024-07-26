ae28377450637331d5f895b5c74657604699e56c
savameta <hoando.dev@gmail.com>
2024-07-26 10:17:03 +0700

    docs: update deployment

--- app/page.tsx
+++ app/page.tsx
@@ -1,25 +1,22 @@
-import "./App.css";
-import { useState, useEffect } from "react";
+"use client";
+import { useEffect, useState } from "react";
 import { Unity, useUnityContext } from "react-unity-webgl";
 
-function App() {
+export default function Home() {
   const { unityProvider } = useUnityContext({
-    loaderUrl: "../build/WebGL Builds.loader.js",
-    dataUrl: "../build/WebGL Builds.data",
-    frameworkUrl: "../build/WebGL Builds.framework.js",
-    codeUrl: "../build/WebGL Builds.wasm",
+    loaderUrl: "/build/WebGL Builds.loader.js",
+    dataUrl: "/build/WebGL Builds.data",
+    frameworkUrl: "/build/WebGL Builds.framework.js",
+    codeUrl: "/build/WebGL Builds.wasm",
   });
 
-  const [devicePixelRatio, setDevicePixelRatio] = useState(
-    window.devicePixelRatio
-  );
-  const [isMobile, setIsMobile] = useState(window.innerWidth <= 768);
-  const [dimensions, setDimensions] = useState({
-    width: window.innerWidth,
-    height: window.innerHeight,
-  });
+  const [devicePixelRatio, setDevicePixelRatio] = useState(1);
+  const [dimensions, setDimensions] = useState({ width: 375, height: 667 });
 
   useEffect(() => {
+    // Set initial states after component mounts
+    setDevicePixelRatio(window.devicePixelRatio);
+
     // Function to update the device pixel ratio
     const updateDevicePixelRatio = () => {
       setDevicePixelRatio(window.devicePixelRatio);
@@ -27,8 +24,7 @@ function App() {
 
     // Function to update the dimensions of the Unity component
     const updateDimensions = () => {
-      setIsMobile(window.innerWidth <= 768);
-      if (isMobile) {
+      if (window.innerWidth <= 768) {
         setDimensions({
           width: window.innerWidth,
           height: window.innerHeight,
@@ -41,25 +37,23 @@ function App() {
       }
     };
 
+    // Initial dimensions setup
+    updateDimensions();
+
     // Media matcher for device pixel ratio changes
     const mediaMatcher = window.matchMedia(
-      `screen and (resolution: ${devicePixelRatio}dppx)`
+      `screen and (resolution: ${window.devicePixelRatio}dppx)`
     );
     mediaMatcher.addEventListener("change", updateDevicePixelRatio);
 
     // Event listener for window resize
     window.addEventListener("resize", updateDimensions);
 
-    // Initial dimensions setup
-    updateDimensions();
-
     return () => {
       mediaMatcher.removeEventListener("change", updateDevicePixelRatio);
       window.removeEventListener("resize", updateDimensions);
     };
-  }, [devicePixelRatio, isMobile]);
-
-  console.log(devicePixelRatio);
+  }, []);
 
   return (
     <Unity
@@ -69,5 +63,3 @@ function App() {
     />
   );
 }
-
-export default App;
